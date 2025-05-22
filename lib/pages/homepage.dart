460f9f0b8b2b745da58133e81a40d74eae2950a2
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-05-23 07:00:34 +0800

    Add Homepage

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _cards = <Widget>[
+    Card(
+      child: Center(
+        child: Text('Card 1'),
+      ),
+    ),
+    Card(
+      child: Center(
+        child: Text('Card 2'),
+      ),
+    ),
+    Card(
+      child: Center(
+        child: Text('Card 3'),
+      ),
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Homepage'),
+      ),
+      body: Center(
+        child: _cards[_selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
